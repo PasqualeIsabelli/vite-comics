4ac2495701360cc06e9a5d3cd8db6583af9142a1
PasqualeIsabelli <pasquale0395@gmail.com>
2023-08-22 14:24:19 +0200

    initial files created

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import TheHeader from './components/TheHeader.vue';
+import TheFooter from './components/TheFooter.vue';
+import TheBanner from './components/TheBanner.vue';
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+    TheBanner
+  }
+}
+</script>
+
+
+<template>
+  
+</template>
+
+
+<style scoped>
+
+</style>
