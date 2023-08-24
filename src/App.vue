9021b0a84a83093843e5bda00fc920eafed99afe
PasqualeIsabelli <pasquale0395@gmail.com>
2023-08-24 15:03:03 +0200

    add card content

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,19 @@
 import TheHeader from './components/TheHeader.vue';
 import TheFooter from './components/TheFooter.vue';
 import NavBar from './components/NavBar.vue';
-import Jumbo from './components/Jumbo.vue';
-import Card from './assets/db/dc-comics.json';
+import CardContainer from './components/CardContainer.vue';
+import cardList from './assets/db/dc-comics.json';
 
 export default {
   components: {
     TheHeader,
     TheFooter,
     NavBar,
-    Jumbo
+    CardContainer
   },
   data() {
     return {
-      Card,
+      cardList,
     }
   }
 }
@@ -27,7 +27,21 @@ export default {
   </header>
   
   <main>
-    <Jumbo></Jumbo> <!--INSERIRE ATTR.--> 
+    <div class="bg-dark pb-5">
+      <div class="jumbo-box"></div>
+      <div class="series-container">
+        <div class="series"><h2 class="text-white fw-bold">CURRENT SERIES</h2></div>
+      </div>
+      <div class="card-content row row-cols-6">
+        <div class="col" v-for="(card, i) in cardList" :key="`${i}`">
+          <CardContainer :img-src="card.thumb" :title="card.series"></CardContainer> <!--:price="card.price" :type="card.type"-->
+        </div>
+      </div>
+      
+      <div class="text-center">
+        <button type="button" class="btn btn-primary fs-4 fw-bold rounded-0">LOAD MORE</button>
+      </div>
+    </div>
     <NavBar></NavBar>
   </main>
 
@@ -38,5 +52,31 @@ export default {
 
 
 <style lang="scss" scoped>
+@use '../src/styles/partials/variables' as *;
 
+.jumbo-box {
+  height: 450px;
+  background-image: url(../src/assets/img/jumbotron.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.series-container {
+  margin: 0 300px;
+  position: relative;
+  .series {
+  background-color: $color-primary;
+  display: inline-block;
+  padding: 1rem 1.5rem;
+  position: absolute;
+  top: -40px;
+  }
+}
+.card-content {
+  margin: 0 300px;
+  
+  button {
+    margin-top: 620px; /*controllare se va bene cosi???? */
+    padding: .5rem 4rem;
+  }
+}
 </style>
---
+++ src/components/CardContainer.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  props: {
+    imgSrc: String,
+    title: String,
+    price: Number,
+    type: String,
+  }
+}
+</script>
+
+
+<template>
+  <div class="card border-0 h-100">
+    <img :src="imgSrc">
+    <div class="card-body text-white fs-4">
+      {{ title.toUpperCase() }}
+    </div>
+    <div class="overlay">
+      <span>Prezzo: {{ price }}</span>
+      <span>Genere: {{ type }}</span>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+.card {
+  margin: 5rem 0 4rem 0;
+  background-color: transparent;
+  height: 100px;
+  .overlay {
+    background-color: rgba(255, 255, 255, 0.5);
+    opacity: 0;
+  }
+  &.hover{
+    .overlay {
+      opacity: 1;
+    }
+  }
+}
+</style>
